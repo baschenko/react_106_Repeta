97df0168090e108674f018d054b61ad6ef189ae1
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-09-22 15:15:21 +0300

    add LocalStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import balanceReducer from "./balanceSlice";
 import localeReducer from "./localeReducer";
 
+const persistConfig = {
+  key: "user-balance",
+  storage,
+  whitelist: ["value"],
+};
+
+const persistedBalaceReducer = persistReducer(persistConfig, balanceReducer);
+
+const persistLangConfig = {
+  key: "app-lang",
+  storage,
+  whitelist: ["lang"],
+};
+
+const persistedLocaleReducer = persistReducer(persistLangConfig, localeReducer);
+
 export const store = configureStore({
   reducer: {
-    balance: balanceReducer,
-    locale: localeReducer,
+    balance: persistedBalaceReducer,
+    locale: persistedLocaleReducer,
   },
 });
 
-// const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "balance/deposit":
-//       return {
-//         ...state,
-//         balance: {
-//           value: state.balance.value + action.payload,
-//         },
-//       };
-
-//     case "balance/withdraw":
-//       return {
-//         ...state,
-//         balance: {
-//           value: state.balance.value - action.payload,
-//         },
-//       };
-//     case "locale/changeLang":
-//       return {
-//         ...state,
-//         locale: {
-//           lang: action.payload,
-//         },
-//       };
-//     default:
-//       return state;
-//   }
-// };
+export const persistor = persistStore(store);
